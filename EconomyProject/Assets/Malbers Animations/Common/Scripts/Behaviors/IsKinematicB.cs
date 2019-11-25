e4e702da0122516f75731a8bafd063fc505ddaf2
Taikatou <conorstephens@gmail.com>
2019-11-25 11:23:52 +0000

    add content

---
+++ EconomyProject/Assets/Malbers Animations/Common/Scripts/Behaviors/IsKinematicB.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace MalbersAnimations
+{
+    public class IsKinematicB : StateMachineBehaviour
+    {
+        public enum OnEnterOnExit { OnEnter, OnExit}
+        public OnEnterOnExit SetKinematic;
+        public bool isKinematic;
+
+        Rigidbody rb;
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            rb = animator.GetComponent<Rigidbody>();
+            if (SetKinematic == OnEnterOnExit.OnEnter)
+            {
+                rb.isKinematic = isKinematic;
+            }
+        }
+
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (SetKinematic == OnEnterOnExit.OnExit)
+            {
+                rb.isKinematic = isKinematic;
+            }
+        }
+    }
+}
